6adaca233da7368731c0606504ca813aa7173bba
StevC6 <stevencalahorrano@gmail.com>
2024-06-14 01:05:33 -0500

    add notes file

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ if hist_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+disp_button = st.button('Construir diagrama de dispersion')
+if disp_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig2 = px.scatter(car_data, x="odometer", y="price")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
